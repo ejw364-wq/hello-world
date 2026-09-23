5df66d5191dfa70143e2025aa0114bf0111a6a54
Evan Westenskow <ejw364@nau.edu>
2026-09-23 19:57:34 +0000

    Changed output of helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#This is a standard hello world program
+
+//This is a standard hello world program
+
 int main() {
-  printf("Hello World!\n");
+  printf("Hello world!\n");
   return 0;
 }
